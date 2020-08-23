f79d576c2ac5f6e489b27fe0b8faea68d2ba3612
rioramski <rio.ramski@ilcs.co.id>
2020-08-23 13:29:56 +0700

    Adding domain

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,31 @@
 package users
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/ramski29/bookstore_users-api/domain/user"
+	"github.com/ramski29/bookstore_users-api/services"
+	"github.com/ramski29/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implemented Me!")
+	var user user.User
+	fmt.Println(user)
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO: handle user creation error
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,9 @@
 package services
+
+import (
+	"github.com/ramski29/bookstore_users-api/domain/user"
+)
+
+func CreateUser(user user.User) (*user.User, error) {
+	return &user, nil
+}
